135d2137ea9a032d3bdcf2aa15bf83a90c8250e7
Brennen Murphy <brennenmurph@pm.me>
2024-01-31 08:38:23 -0500

    works on OFW using libXploit

--- regedit_prx/clean_kprx.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-path=C:\pspsdk\bin
-make clean
-del /q *.S
--- regedit_prx/regedit.h
+++
@@ -1,3 +0,0 @@
-int prxIdStorageLookup(u16 key, u32 offset, void *buf, u32 len);
-int prxIdStorageReadLeaf(u16 key, void *buf);
-int prxIdStorageWriteLeaf(u16 key, void *buf);
